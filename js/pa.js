99bc949f62338c56f9c79fc18a05221ec2de915b
Rakesh Divekar <Rakesh@Stanelys-MacBook-Pro.local>
2017-07-05 18:15:04 +0530

    added page specific containers

--- js/pa.js
+++ js/pa.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	$('.close-btn').click(function(){
 		console.log('dsfsd');
 		$(this).parents('tr').hide();
-	})	
+	});
+	$('.close-btn').click(function() {
+	    bootbox.confirm("Are you sure want to delete?", function(result) {
+	      alert("Confirm result: " + result);
+	    });
+	  });
 })
-
