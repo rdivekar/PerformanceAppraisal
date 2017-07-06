535c0b41540278ff672de54113301fd1e2c6430e
Rakesh Divekar <Rakesh@Stanelys-MacBook-Pro.local>
2017-07-06 11:20:35 +0530

    pa.css added after bootstrap.css

--- js/pa.js
+++ js/pa.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 	$('.close-btn').click(function(){
-		console.log('dsfsd');
+	//	console.log('dsfsd');
 		$(this).parents('tr').hide();
 	});
-	$('.close-btn').click(function() {
-	    bootbox.confirm("Are you sure want to delete?", function(result) {
-	      alert("Confirm result: " + result);
-	    });
-	  });
+	
 })
