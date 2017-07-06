a0235e042707fe06444cef786ecc858b036f5205
Rakesh Divekar <Rakesh@Stanelys-MacBook-Pro.local>
2017-07-06 16:57:27 +0530

    comments for manager added and add link added

--- js/pa.js
+++ js/pa.js
@@ -1,7 +1,49 @@
 $(document).ready(function(){
+	var pageHeading = $('body h2').html();
+
+	var deletion = '<div id="deleteAlert" class="modal fade" role="dialog">\
+					  <div class="modal-dialog">\
+					    <div class="modal-content">\
+					      <div class="modal-header">\
+					        <!--<button type="button" class="close" data-dismiss="modal">&times;</button>-->\
+					        <h4 class="modal-title">Delete this "'+ pageHeading + '"</h4>\
+					      </div>\
+					      <div class="modal-body">\
+					        <p>Some text in the modal.</p>\
+					      </div>\
+					      <div class="modal-footer">\
+					        <button type="button" class="btn secondary" data-dismiss="modal">Cancel</button>\
+					        <button type="button" class="btn primary" data-dismiss="modal">Yes</button>\
+					      </div>\
+					    </div>\
+					  </div>\
+					</div><style>.modal-title span{display: none;}';
+	// modal html content for deletion starts here 
+	$('body').append(deletion);
+	$('.close-btn').attr("data-toggle", "modal").attr('data-target', '#deleteAlert');
+	
 	$('.close-btn').click(function(){
-	//	console.log('dsfsd');
-		$(this).parents('tr').hide();
+		console.log($(this));
+		var delet = false;	
+		//console.log(deletion);
+		// $(this).parents('tr').remove();
+		// $(this).parents('.activities-content').remove();
+		// $(this).parents('.developmental-needs-actions').remove();
+	});	
+
+
+
+	$('#deleteAlert .modal-footer button').on('click', function(e){
+		console.log($(this));
+		if ($(this).hasClass('primary')) {
+			console.log('primary clicked');
+			delet = true;
+		}
+		else {
+			console.log('secondary clicked');
+			delet = false;
+		}
 	});
+
 	
 })
